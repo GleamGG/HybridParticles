6f5247f5b4d1d659fe8375256165e65a88297d5e
Hydro <baksteengamer@gmail.com>
2023-12-08 11:32:12 +0200

    Test Commit/Push

--- src/main/java/xyz/hybridmc/hybridparticles/HybridParticles.java
+++ src/main/java/xyz/hybridmc/hybridparticles/HybridParticles.java
@@ -20,6 +20,6 @@ public final class HybridParticles extends JavaPlugin {
         pm.registerEvents(new Quit(), this);
         pm.registerEvents(new Movement(), this);
 
-        this.getCommand("trail").setExecutor(new Trails());
+        this.getCommand("trails").setExecutor(new Trails());
     }
 }
